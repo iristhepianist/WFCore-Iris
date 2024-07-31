a7646dffbc4a3c0b95ffb650a4925408fd018384
last8kings <97848034+last8kings@users.noreply.github.com>
2024-07-31 03:14:14 -0400

    edit mixin

--- src/main/java/wfcore/mixins/vanilla/CombatRulesMixin.java
+++ src/main/java/wfcore/mixins/vanilla/CombatRulesMixin.java
@@ -5,15 +5,28 @@ import net.minecraft.util.CombatRules;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static org.spongepowered.asm.mixin.injection.callback.LocalCapture.CAPTURE_FAILSOFT;
 
 @Mixin(CombatRules.class)
 public abstract class CombatRulesMixin {
 
-    // here I just copied over the entire method, hope it won't cause issues.
-    @ModifyReturnValue(method = "getDamageAfterAbsorb", at = @At(value = "RETURN"))
-    private static float getDamageAfterAbsorb(float damage, float totalArmor, float toughnessAttribute) {
+    /*
+    public static float getDamageAfterAbsorb(float damage, float totalArmor, float toughnessAttribute)
+    {
         float f = 2.0F + toughnessAttribute / 4.0F;
-        float f1 = MathHelper.clamp(totalArmor - damage / f, totalArmor * 0.2F, 25.0F);
+        float f1 = MathHelper.clamp(totalArmor - damage / f, totalArmor * 0.2F, 20.0F);
         return damage * (1.0F - f1 / 25.0F);
+    }
+    */
+    // here I just copied over the entire method, hope it won't cause issues.
+    //@ModifyReturnValue(method = "getDamageAfterAbsorb", at = @At(value = "RETURN"))
+    @Inject(method = "getDamageAfterAbsorb(FFF)F", at = @At("RETURN"), cancellable = true, locals = CAPTURE_FAILSOFT)
+    private static void getDamageAfterAbsorb(float damage, float totalArmor, float toughnessAttribute, CallbackInfoReturnable<Float> ci, float f) {
+        //float f = 2.0F + toughnessAttribute / 4.0F;
+        float f1 = MathHelper.clamp(totalArmor - damage / f, totalArmor * 0.2F, 25.0F);
+        ci.setReturnValue(damage * (1.0F - f1 / 25.0F));
     }
 }
