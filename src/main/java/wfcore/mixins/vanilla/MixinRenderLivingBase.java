2c5a328522c57e0ca814869ef8b5bdbc226876e7
last8kings <97848034+last8kings@users.noreply.github.com>
2024-09-01 14:00:13 -0400

    change mixin

--- src/main/java/wfcore/mixins/vanilla/MixinRenderLivingBase.java
+++ src/main/java/wfcore/mixins/vanilla/MixinRenderLivingBase.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderer.class)
-public class MixinRenderLivingBase() {
+public class MixinRenderLivingBase {
     @Inject(method = "drawNameplate", at=@At("TAIL"))
     private void DisableDepthBuffer(){
         GlStateManager.disableDepth();
