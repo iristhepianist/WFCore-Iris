0991f7f2c0180e2857cc5ed1f88983972a0c2364
lrdmtns <lrdmtns@gmail.com>
2025-01-22 16:43:43 +0000

    guh

--- src/main/java/wfcore/common/recipe/chain/SteamWiremillRecipes.java
+++ src/main/java/wfcore/common/recipe/chain/SteamWiremillRecipes.java
@@ -1,13 +1,5 @@
 package wfcore.common.recipe.chain;
 
-import gregtech.api.unification.OreDictUnifier;
-import gregtech.api.unification.material.Materials;
-import gregtech.api.unification.ore.OrePrefix;
-
-import static gregtech.api.unification.ore.OrePrefix.wireFine;
-import static gregtech.api.unification.ore.OrePrefix.wireGtSingle;
-import static wfcore.api.recipes.WFCoreRecipeMaps.STEAM_WIREMILL_RECIPES;
-
 public class SteamWiremillRecipes {
     public static void init() {
 
