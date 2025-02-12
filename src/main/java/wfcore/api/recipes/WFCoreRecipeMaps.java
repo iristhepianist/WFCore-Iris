a610d885cc9b23ce56f947627f21350be121825f
Wht/Mld <Lrdmtns@gmail.com>
2025-02-12 16:46:27 +0000

    Revert "I'm going to Kill Myself."
    This reverts commit 8b5d7ca97eafb815588d5426f9a36e6a81b1b2d7.

--- src/main/java/wfcore/api/recipes/WFCoreRecipeMaps.java
+++ src/main/java/wfcore/api/recipes/WFCoreRecipeMaps.java
@@ -1,27 +1,45 @@
 package wfcore.api.recipes;
 
+
 import gregtech.api.gui.GuiTextures;
-import gregtech.api.gui.widgets.ProgressWidget;
 import gregtech.api.recipes.RecipeMap;
-import gregtech.api.recipes.RecipeMaps;
-import gregtech.api.recipes.builders.FuelRecipeBuilder;
+
 import gregtech.api.recipes.builders.PrimitiveRecipeBuilder;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
-import gregtech.core.sound.GTSoundEvents;
 
 public class WFCoreRecipeMaps {
-    public static final RecipeMap<SimpleRecipeBuilder> PROJECTOR_RECIPES = new RecipeMap<>("projector", 4, 4, 4, 4, new SimpleRecipeBuilder(), false)
-            .setProgressBar(GuiTextures.PROGRESS_BAR_CIRCUIT, ProgressWidget.MoveType.HORIZONTAL)
-            .setSound(GTSoundEvents.ASSEMBLER); //Temporary Sound
 
-    public static final RecipeMap<SimpleRecipeBuilder> STEAM_WIREMILL_RECIPES = new RecipeMap<>("steamwiremill", 1, 1, 0, 0, new SimpleRecipeBuilder(), false)
-            .setProgressBar(GuiTextures.PROGRESS_BAR_WIREMILL, ProgressWidget.MoveType.HORIZONTAL);
-    public static final RecipeMap<SimpleRecipeBuilder> Large_Blast_Furnace = new RecipeMap<>("steamwiremill", 2, 1, 0, 1, new SimpleRecipeBuilder(), false)
-            .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, ProgressWidget.MoveType.HORIZONTAL);
-    public static final RecipeMap<SimpleRecipeBuilder> StrandCaster = new RecipeMap<>("steamwiremill", 1, 1, 2, 1, new SimpleRecipeBuilder(), false)
-            .setProgressBar(GuiTextures.PROGRESS_BAR_WIREMILL, ProgressWidget.MoveType.HORIZONTAL);
+    public static final RecipeMap<SimpleRecipeBuilder> PROJECTOR_RECIPES = new RecipeMapBuilder<>("projector",
+            new SimpleRecipeBuilder())
+            .itemInputs(3)
+            .itemOutputs(3)
+            .fluidInputs(5)
+            .fluidOutputs(2)
+            .progressBar(GuiTextures.PROGRESS_BAR_ARROW_MULTIPLE)
+            .build();
+
+    public static final RecipeMap<SimpleRecipeBuilder> STEAM_WIREMILL_RECIPES = new RecipeMapBuilder<>("steam_wiremill",
+            new SimpleRecipeBuilder())
+            .itemInputs(1)
+            .itemOutputs(1)
+            .fluidInputs(0)
+            .fluidOutputs(0)
+            .progressBar(GuiTextures.PROGRESS_BAR_WIREMILL)
+            .build();
+    public static final RecipeMap<PrimitiveRecipeBuilder> Large_Blast_Furnace = new RecipeMapBuilder<>("large_blast_furnace",
+            new PrimitiveRecipeBuilder())
+            .itemInputs(2)
+            .itemOutputs(1)
+            .fluidInputs(0)
+            .fluidOutputs(1)
+            .progressBar(GuiTextures.PROGRESS_BAR_ARROW)
+            .build();
+    public static final RecipeMap<PrimitiveRecipeBuilder> StrandCaster = new RecipeMapBuilder<>("strand_caster",
+            new PrimitiveRecipeBuilder())
+            .itemInputs(1)
+            .itemOutputs(1)
+            .fluidInputs(2)
+            .fluidOutputs(1)
+            .progressBar(GuiTextures.PROGRESS_BAR_ARROW)
+            .build();
 }
-
-
-
-
